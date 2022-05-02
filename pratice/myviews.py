b2a5742ce37836a23812f3f19acaaf6f76f1ef46
githubPratima <pp235@njit.edu>
2022-05-02 08:22:17 -0400

    uploading miniproject 3

---
+++ pratice/myviews.py
@@ -0,0 +1,130 @@
+import csv
+import os
+
+from flask import Blueprint, request, render_template, flash, url_for
+from flask_login import current_user
+from sqlalchemy.exc import SQLAlchemyError
+
+from calc.MyCalc import MyCalc
+from base_model import db
+from werkzeug.utils import redirect
+
+from .models import SimpleHistory
+
+c = MyCalc()
+mycalc = Blueprint('mycalc', __name__, url_prefix='/mycalc')
+
+@mycalc.route('/history', methods=['GET'])
+def get_history():
+    if not current_user.is_anonymous:
+        results = SimpleHistory.query.filter_by(user_id=current_user.id).order_by(SimpleHistory.created.desc()).limit(
+            10).all()
+        print(results)
+    else:
+        results = []
+    return render_template("calc-history.html", history=results)
+
+@mycalc.route('/delete/<history_id>', methods=['POST'])
+def delete_single_history(history_id):
+    if not current_user.is_anonymous:
+        try:
+            sh = db.session.query(SimpleHistory).filter_by(id=history_id).delete()
+            db.session.commit()
+            print(f"Deleted history {sh}")
+            flash("History deleted", "success")
+        except SQLAlchemyError as e:
+            print(e)
+            flash(str(e), "error")
+            db.session.rollback()
+    else:
+        print("Not authorized")
+        flash("Not authorized to delete other user history", "error")
+    return redirect(url_for("mycalc.get_history"))
+
+
+@mycalc.route('/delete', methods=['POST'])
+def delete():
+    if not current_user.is_anonymous:
+        try:
+            sh = db.session.query(SimpleHistory).filter_by(user_id=current_user.id).delete()
+            db.session.commit()
+            print("Deleted all history for this user")
+            flash("All history of user deleted", "success")
+        except SQLAlchemyError as e:
+            print(e)
+            flash(str(e), "error")
+            db.session.rollback()
+    else:
+        print("Not authorized")
+        flash("Not authorized to delete other user history", "error")
+    return redirect(url_for("mycalc.get_history"))
+
+
+@mycalc.route('/upload', methods=['GET', 'POST'])
+def upload_csv():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        print(uploaded_file.filename)
+        uploaded_file.save(uploaded_file.filename)
+        try:
+            with open(uploaded_file.filename) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=",")
+                line_count = 0
+                for row in csv_reader:
+                    if line_count == 0:
+                        print(f'Column names are {", ".join(row)}')
+                        line_count += 1
+                    else:
+                        print(row)
+                        op = row.pop(0)
+                        c.calc(op, row)
+        except Exception as e:
+            print('An exception occurred: {}'.format(e))
+        finally:
+            os.remove(uploaded_file.filename)
+            print("Deleted uploaded file")
+    return redirect(url_for("mycalc.do_calc"))
+
+
+
+
+@mycalc.route('/', methods=['GET', 'POST'])
+def do_calc():
+    # c = calc.MyCalc.AdvMyCalc()
+    checks = c.ops
+    print(c.ops)
+    data = request.form
+    iSTR = data.get("eq")
+    loadHistory = data.get("loadHistory", 0, type=int) > 0
+    if iSTR is not None:
+        r = "UNSUPPORTED"
+        if loadHistory:
+            r = data.get("result")
+            c.ans = c._as_number(r)
+        else:
+            for check in checks:
+                print(str(checks) + " " + str(iSTR))
+                if check in iSTR:
+                    nums = iSTR.split(check)
+                    print(nums)
+                    try:
+                        r = c.calc(check, *nums)
+                        print("R is " + str(r))
+                    except:
+                        r = "ERROR"
+                    if not current_user.is_anonymous:
+                        sh = SimpleHistory(eq=iSTR, result=str(r), user_id=current_user.id)
+                        db.session.add(sh)
+                        try:
+                            db.session.commit()
+                            print("Recorded calculation history")
+                            flash("Recorded calculation history ")
+                        except SQLAlchemyError as e:
+                            print(e)
+                            flash(str(e), "error")
+                            db.session.rollback()
+                    return render_template("my_calc.html", result=r, eq=iSTR)
+            print("The action you tried is not supported, please try again")
+            return render_template("my_calc.html", result="UNSUPPORTED", eq=iSTR)
+    return render_template("my_calc.html", result="")
+
---
+++ sample_app/calc/MyCalc.py
@@ -0,0 +1,296 @@
+
+class MyCalc:
+    ans = 0
+    ops = ["+", "-", "x", "/", "square", "sqrt", "smean", "median", "sstd_dev", "mode", "svariance"]
+    def _is_float(self, val):
+        try:
+            val = float(val)
+            return True
+        except:
+            return False
+
+    def _is_int(self, val):
+        try:
+            val = int(val)
+            return True
+        except:
+            return False
+
+    def _as_number(self, val):
+        if self._is_int(val):
+            return int(val)
+        elif self._is_float(val):
+            return float(val)
+        else:
+            raise Exception("Not a number")
+
+    def calc(self, op, *in_nums):
+        print(op)
+        print(in_nums)
+        print("Hello")
+        nums = in_nums[1].split(",")
+
+        if len(nums) == 0 or len(nums) == 1:
+            nums = list(in_nums)
+            print(f"Regular list nums: {nums}")
+        else:
+            print(f"List from tuple nums: {nums}")
+        print("Bye")
+        if op == "+":
+            nums = in_nums
+            print("add nums{}".format(nums))
+            return self.add(nums[0],nums[1])
+        elif op == "-":
+            nums = in_nums
+            print("Hello")
+            print("sub nums{}".format(nums))
+            return self.sub(nums[0],nums[1])
+        elif op == "/":
+            nums = in_nums
+            print("div nums{}".format(nums))
+            return self.div(nums[0],nums[1])
+        elif op == "x":
+            nums = in_nums
+            print("add mult{}".format(nums))
+            return self.mult(nums[0],nums[1])
+        elif op == "square":
+            nums.pop(0)
+            print("square nums{}".format(nums))
+            return self.square(*nums)
+        elif op == "sqrt":
+            nums.pop(0)
+            print("sqrt nums{}".format(nums))
+            return self.sqrt(*nums)
+
+        elif op == "smean":
+             nums.pop(0)
+             print("smean nums{}".format(nums))
+             return self.smean(*nums)
+        elif op == "median":
+            nums.pop(0)
+            print("median nums{}".format(nums))
+            return self.median(*nums)
+        elif op == "sstd_dev":
+            nums.pop(0)
+            print("sstd_dev nums{}".format(nums))
+            return self.sstd_dev(*nums)
+        elif op == "mode":
+            nums.pop(0)
+            print("mode nums{}".format(nums))
+            return self.mode(*nums)
+        elif op == "svariance":
+            nums.pop(0)
+            print("svariance nums{}".format(nums))
+            return self.svariance(*nums)
+
+
+
+
+    def add(self, num1, num2):
+        if num1 == "ans":
+            return self.add(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1 + num2
+        return self.ans
+
+    def sub(self, num1, num2):
+        if num1 == "ans":
+            return self.sub(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1 - num2
+        return self.ans
+
+    def mult(self, num1, num2):
+        if num1 == "ans":
+            return self.mult(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1 * num2
+        return self.ans
+
+    def div(self, num1, num2):
+        if num1 == "ans":
+            return self.div(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            if num2 == 0:
+                print("Can't divide by zero, sorry")
+            else:
+                self.ans = num1 / num2
+        return self.ans
+
+
+    def sqrt(self,N):
+        if self._as_number(N) < 0:
+            print('Square root of negative number does not exist!')
+            return
+        else:
+            sqrt = self._as_number(N)**0.5  #this code gives square root of the number
+            print('Square root of number {}: {}'.format(N,sqrt))
+            return sqrt
+
+
+
+    def square(self, n):
+        if self._as_number(n) < 0:
+            return
+        else:
+            return self._as_number(n) ** 2  #this code gives square of the number
+
+    def smean(self, *in_data):
+        data = list(in_data)
+        print(data)
+        n = len(data)
+        for i in range(n):
+            data[i] = self._as_number(data[i])
+        smean = sum(data) / n
+        return smean
+
+
+    def median(self, *in_L):
+        L= list(in_L)
+        print(L)
+        L.sort()
+        lLen = len(L)
+        half = int(lLen / 2)  #this code gives median of the numbers
+        if lLen % 2 != 0:
+            median = L[half]
+        else:
+            left = self._as_number(L[half - 1])
+            right = self._as_number(L[half])
+            # median = L[half - 1] + L[half]
+            median = left + right
+            median = median / 2
+        return median
+
+
+    def mode(self, *in_data):
+        data = list(in_data)
+        print(data)
+        counts = {}
+        for item in data:
+            num = self._as_number(item)
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1  # These codes helps to find out the mode
+        return counts
+
+
+    def svariance(self, *in_data):
+        data = list(in_data)
+        print(data)
+        n= len(data)
+        for i in range(n):
+            data[i] = self._as_number(data[i])
+        mean = sum(data) / n
+
+        deviations = [(self._as_number(x) - mean) ** 2 for x in data]
+        svariance = sum(deviations) / n #this code gives us the sample variance of numbers.
+        return svariance
+
+
+    def sstd_dev(self, *in_ls):
+        ls = list(in_ls)
+        print(ls)
+        n = len(in_ls)
+        for i in range(len(ls)):
+            ls[i] = self._as_number(ls[i])
+        mean = sum(ls) / n
+        var = sum((x - mean) ** 2 for x in ls) / n
+        sstd_dev = var ** 0.5   #this code gives us the standard deviation of numbers.
+        return sstd_dev
+
+
+
+
+
+if __name__ == '__main__':
+
+    is_running = True
+    iSTR = input("Are you ready?")
+    calc = MyCalc()
+    ops = ["+", "-", "x", "/", "square", "sqrt", "smean", "median", "sstd_dev", "mode", "svariance"]
+    print(calc)
+    if iSTR == "yes":
+        while is_running:
+            iSTR = input("What is your eq:")
+            if iSTR == "quit" or iSTR == "q":
+                is_running = False
+            else:
+                print("Your eq was " + str(iSTR))
+                if "+" in iSTR:
+                    nums = iSTR.split("+")
+                    r = calc.add(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done before - check to hanlde negative values
+                elif "/" in iSTR:
+                    nums = iSTR.split("/")
+                    r = calc.div(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+                elif "*" in iSTR or "x" in iSTR:
+                    nums = iSTR.split("*") if "*" in iSTR else iSTR.split("x")
+                    r = calc.mult(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done last so negative numbers work
+                elif "-" in iSTR:
+                    nums = iSTR.split("-")
+                    r = calc.sub(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+                elif "smean" in iSTR:
+                    print("doing smean")
+                    nums = iSTR.split("smean")[1].strip("[").strip("]").split(",")
+                    print("nums after split")
+                    print(nums)
+
+                    r = calc.smean(nums)
+                    print("R is " + str(r))
+
+
+                elif "median" in iSTR:
+                    print("doing median")
+                    nums = iSTR.split("median")[1].strip("[").strip("]").split(",")  # assumes a comma list of values for median
+                    r = calc.median(nums)
+                    print("R is " + str(r))
+
+                elif "mode" in iSTR:
+                    print("doing mode")
+                    nums = iSTR.split("mode")[1].strip("[").strip("]").split(",")
+                    r = calc.mode(nums)
+                    print("R is " + str(r))
+
+                elif "sstd_dev" in iSTR:
+                    print("doing sstd_dev")
+                    nums = iSTR.split("sstd_dev")[1].strip("[").strip("]").split(",")
+                    r = calc.sstd_dev(nums)
+                    print("R is " + str(r))
+
+                elif "svariance" in iSTR:
+                    print("doing svariance")
+                    nums = iSTR.split("svariance")[1].strip("[").strip("]").split(",")
+                    r = calc.svariance(nums)
+                    print("R is " + str(r))
+                elif "sqrt" in iSTR:
+                    nums = iSTR.split("sqrt")[1].strip("[").strip("]")
+                    r = calc.sqrt(nums)
+                    print("R is " + str(r))
+                elif "square" in iSTR:
+                    nums = iSTR.split("square")[1].strip("[").strip("]")
+                    r = calc.square(nums)
+                    print("R is " + str(r))
+
+                    # etc
+
+    else:
+
+        print("Good bye")
+
+
